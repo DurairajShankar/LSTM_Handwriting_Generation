639fc233d89d40aa0f72a85bfe531962f7fd40ce
Durairaj <40930529+durairaj98@users.noreply.github.com>
2020-03-24 18:20:32 +0530

    Update FrontEndController.py

--- controller/FrontEndController.py
+++ controller/FrontEndController.py
@@ -46,50 +46,13 @@ def FrontEndController(app, request, db, render_template, session, redirect):
         if request.form['user_otp'] == request.form['actual_otp']:
             data='results/result.png'
             return render_template('OTP_result.html', data=data)
-        return 'Unauthorized'
+        return redirect('/otphtml_page')
 
     @app.route('/result_page')
     def result_page():
         data='results/result.png'
         return render_template('result.html', data=data)
     
-    @app.route('/addtrainablemodel_page')
-    def addTrainableModel_page():
-        if not session.get('authenticated'):
-            return render_template('login.html')
-        elif not session.get('role')=='admin':
-            return render_template('index.html')
-        return render_template('addtrainablemodel.html')
-    
-    @app.route('/addnontrainablemodel_page')
-    def addNonTrainableModel_page():
-        if not session.get('authenticated'):
-            return render_template('login.html')
-        elif not session.get('role')=='admin':
-            return render_template('404.html')
-        return render_template('addnontrainablemodel.html')
-
-    @app.route('/admin_dashboard')
-    def adminDashboard_page():
-        if not session.get('authenticated'):
-            return render_template('login.html')
-        elif not session.get('role')=='admin':
-            return render_template('404.html')
-        return render_template('admin_dashboard.html')
-
-    @app.route('/dashboard_page')
-    def dashboard_page():
-        if not session.get('authenticated'):
-            return render_template('login.html')
-        if not session.get('confirmed'):
-            return render_template('unconfirmed_dashboard.html')
-        user = db.getUserByEmail(session['email'])
-        models = db.getModelUserCountDetails(session['email'])
-        request_models  = db.getModelUserRequestDetails(session['email'])
-        api = db.getModelUserApiDetails(session['email'])
-        model_desc = db.getAllTrainableModelDescription()
-        datasets = db.getDatasetsByEmail(session['email'])
-        return render_template('dashboard.html',user=user,datasets=datasets,model_desc=model_desc,api=api,hostname='localhost:5000',models=models,request=request_models)
     
     @app.route('/updatepassword_page')
     def updatePassword_page():
